1b08c9f44bf8650329f39563c3b0101f178b07a0
Rami Qanber <Rami.alqanbar@gmail.com>
2021-05-23 13:52:32 +0200

    dep

---
+++ src/compnents/navigation/desktopnavigation/DesktopNavigation.tsx
@@ -0,0 +1,21 @@
+import './DesktopNavigation.css'
+import {useHistory} from 'react-router-dom'
+import logo from '../../../shared/images/logo.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+export const DesktopNavigation = () => {
+    const history = useHistory()
+    return (
+        <div className="nav-bar">
+           
+            <img className="nav-logo" src={logo} alt={'Error Logo'} onClick={() => history.push('/')}/>
+            <span className="nav-home" onClick={() => history.push('/home')}>Home</span>
+            <span className="nav-products" onClick={() => history.push('/products')}>Products</span>
+            <span className="nav-about" onClick={() => history.push('/about')}>About</span>
+            <span className="nav-contact" onClick={() => history.push('/contact')}>Contact</span>
+            <FontAwesomeIcon className="nav-cart" onClick={() => history.push('/cart')} icon={faShoppingCart} size="lg"/>
+            <span className="nav-signin" onClick={() => history.push('/signin')}>Login</span>
+
+        </div>
+    )
+}
---
+++ src/shared/api/E_COMMERCE_API.ts
@@ -0,0 +1,8 @@
+import Axios from 'axios'
+const developmentAPI = 'http://localhost:3001'
+
+const E_COMMERCE_API = Axios.create({
+    baseURL: developmentAPI
+})
+
+export default E_COMMERCE_API 
